535b3137c0fb0ed06f73364a538cf193bbc8d214
Rami1940 <ahadramijr@gmail.com>
2023-07-07 11:10:28 -0400

    swap numbers with an intermediate

---
+++ src/main/java/Java_Interview_Questions/Swap_Two_Integers_Without_Creating_additional_variables.java
@@ -0,0 +1,34 @@
+package Java_Interview_Questions;
+
+public class Swap_Two_Integers_Without_Creating_additional_variables {
+
+    // swap two integers without creating and additional variables
+
+
+
+
+
+
+
+
+
+
+
+
+   /* public static void main(String[] args) {
+        int first=1;
+        int second=2;
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+
+        first=first+second; // first holds first and second
+        second=first-second; // first and second - second is just first
+        first=first-second; // first still has first and second so take away second to keep first
+
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+
+    }
+
+    */
+}
